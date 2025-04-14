ae3c9f2915eb82f11b924ba68e0e4a3e803f240c
DevBrianKE <kipchumbabrian47@gmail.com>
2025-04-14 20:06:06 +0300

    Fix: removed submodule and added src/data as regular folder

---
+++ src/data/sampleExpenses.js
@@ -0,0 +1,21 @@
+const sampleExpenses = [
+    {
+      id: 1,
+      name: "Light Mamudo",
+      description: "Wednesday's Lunch",
+      category: "food",
+      amount: 100,
+      date: "2025-04-08"
+    },
+    {
+      id: 2,
+      name: "NYC taxes",
+      description: "power taxes",
+      category: "utilities",
+      amount: 2000,
+      date: "2025-04-05"
+    }
+  ];
+  
+  export default sampleExpenses;
+  
